2610fcbb70ff6cd82391db0da6a6019f8b4893db
Matthias Max <matthias.max@bitflower.net>
2021-07-19 20:51:33 +0200

    jest-directly (#17)

--- packages/design-system/stencil.config.ts
+++ packages/design-system/stencil.config.ts
@@ -1,23 +1,33 @@
 import { Config } from '@stencil/core';
 // import nodePolyfills from 'rollup-plugin-node-polyfills';
 import builtins from 'rollup-plugin-node-builtins';
+import replace from '@rollup/plugin-replace';
 
 export const config: Config = {
   namespace: 'design-system',
   outputTargets: [
     {
       type: 'dist',
-      esmLoaderPath: '../loader'
+      esmLoaderPath: '../loader',
     },
     {
-      type: 'docs-readme'
+      type: 'docs-readme',
     },
     {
       type: 'www',
-      serviceWorker: null // disable service workers
-    }
+      serviceWorker: null, // disable service workers
+    },
   ],
-  plugins: [builtins()]
+  plugins: [builtins()],
+  rollupPlugins: {
+    before: [
+      replace({
+        preventAssignment: true,
+        delimiters: ['', ''],
+        '_interopRequireDefault(require("sift"))': "require('sift')",
+      }),
+    ],
+  },
   // testing: {
   //   // transformIgnorePatterns: [
   //   //   '<rootDir>.*(node_modules)(?!.*@ryancavanaugh/pkg.*).*$'
@@ -27,5 +37,11 @@ export const config: Config = {
   //   //   '@ryancavanaugh/(.*)': '<rootDir>/../$1'
   //   //   // '@ryancavanaugh/pkg1': '<rootDir>/../pkg1'
   //   // },
+  //   // resolver: '<rootDir>/jest-resolver.js',
+  //   // moduleDirectories: [
+  //   //   'node_modules',
+  //   //   // '/Users/matthias/Documents/Projekte/bitflower/Case OS/Prototype/v0.2/_Knowledge/StencilJS/stencil-lerna-tests/packages/pkg1',
+  //   //   '../pkg1'
+  //   // ]
   // }
 };
